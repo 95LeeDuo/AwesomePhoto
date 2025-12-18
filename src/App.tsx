4bfb37d8ae97a87c458fb137383dc899f27a76b1
george <george@irm.kr>
2025-12-18 19:14:55 +0900

    chore: project setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className={"text-red-500 bg-amber-50"}>asd</h1>
+      <h1 className={"text-red-500 bg-amber-50"}>asd</h1>
+    </>
+  );
+}
+
+export default App;
