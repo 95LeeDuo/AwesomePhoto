ee52c1f3954f38481165120a2d9ae321a15a1035
lbw3973 <lbw3973@naver.com>
2025-12-21 23:26:48 +0900

    feat: layout 세팅

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import type { PropsWithChildren } from "react";
+import BackgroundBalls from "@/components/Layout/BackgroundBalls.tsx";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main
+      className={
+        "min-h-screen flex flex-col bg-gradient-to-br from-indigo-50 via-white to-purple-50 relative overflow-hidden"
+      }
+    >
+      <BackgroundBalls />
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
