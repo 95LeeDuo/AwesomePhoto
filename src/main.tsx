b8bf92ce65afa98b17d1482548743a5866eddb2c
sylee8048 <water8048@naver.com>
2025-12-29 17:09:06 +0900

    feat: redux 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 import Router from "@/routes/Router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </StrictMode>,
 );
